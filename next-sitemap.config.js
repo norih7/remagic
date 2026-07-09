5ae6726ea14992c92f88514d8d6b814fb1db7f8c
norih <>
2026-07-09 10:15:15 +0900

    fix next-sistemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://remagic.brclover.com", // 実際のドメインに合わせてください
-  generateRobotsTxt: true, // robots.txtも自動生成
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/", // ここをすべて許可にする
+      },
+    ],
+  },
   outDir: "out", // next build 後の出力先に指定
   // ここで除外設定を行います
   exclude: ["/items/*"],
