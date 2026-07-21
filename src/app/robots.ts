612b4d9acde3c9670648e3c6a4595e0ac6b8870c
norih <>
2026-07-22 06:15:38 +0900

    fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/_next/static", "/_next/image"],
       },
       {
         userAgent: "GPTBot",
