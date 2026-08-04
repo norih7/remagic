ad307933c3a61975ca24c5fd78eea2714cbc7c00
nori <nori@norinoMacBook-Air.local>
2026-08-04 21:03:58 +0900

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,16 +2,13 @@
 module.exports = {
   siteUrl: "https://remagic.brclover.com",
   generateRobotsTxt: false,
-  generateLastmod: false,
   outDir: "out",
   exclude: ["/sitemap.xml", "/systems/item/*"],
 
   transform: async (config, path) => {
     return {
       loc: path,
-      changefreq: "daily",
       priority: path === "/" ? 1.0 : 0.7,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
     };
   },
 };
