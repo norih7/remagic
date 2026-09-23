ac37baf12fae6719da313916e6cf3732d7b4826c
nori <nori@norinoMacBook-Air.local>
2026-09-23 19:01:19 +0900

    fix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,18 +1,15 @@
 export async function onRequest(context) {
-  // const url = new URL(context.request.url);
-  // Google Search Console確認ファイルだけは、自動リダイレクトを一切経由せず直接ファイルを返す
-  // if (/^\/google[a-z0-9]+\.html$/i.test(url.pathname)) {
-  //   return context.env.ASSETS.fetch(context.request);
-  // }
-  // if (url.hostname === "remagic.pages.dev") {
-  //   url.hostname = "remagic.brclover.com";
-  //   return new Response(null, {
-  //     status: 301,
-  //     headers: {
-  //       Location: url.toString(),
-  //       "X-Robots-Tag": "noindex",
-  //     },
-  //   });
-  // }
+  const url = new URL(context.request.url);
+
+  if (url.hostname === "remagic.pages.dev") {
+    url.hostname = "remagic.brclover.com";
+    return new Response(null, {
+      status: 301,
+      headers: {
+        Location: url.toString(),
+        "X-Robots-Tag": "noindex",
+      },
+    });
+  }
   return context.next();
 }
